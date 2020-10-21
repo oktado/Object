2f11393b0d59980f80aa8db816da7bbe335e3330
Oktado Putra <oktado.putra@gmail.com>
2020-10-21 07:39:06 +0700

    first

---
+++ script.js
@@ -0,0 +1,12 @@
+let hewan = {
+    nama: 'Kelelawar',
+    kelas: 'Mamalia',
+    'nama lain': 'Chiroptera',
+    makanan: 'herbivora',
+    'penutup tubuh': 'berambut',
+    'berkembang biak': 'melahirkan',
+    tipe: 'nocturnal',
+    
+
+}
+console.log(hewan);
